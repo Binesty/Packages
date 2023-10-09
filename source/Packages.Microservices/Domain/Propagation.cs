1e7b8b0dae2bd209abd314fdfb508749998e8310
Vladimir Matos <vladimir.matos@outlook.com>
2023-10-09 18:57:25 -0300

    feat: create replication class and interface #39

--- source/Packages.Microservices/Domain/Propagation.cs
+++ source/Packages.Microservices/Domain/Propagation.cs
@@ -1,4 +1,5 @@
 ﻿using Packages.Microservices.Messages;
+using System.Linq.Expressions;
 
 namespace Packages.Microservices.Domain
 {
@@ -9,5 +10,14 @@ namespace Packages.Microservices.Domain
         public dynamic? Content { get; set; }
 
         public ulong DeliveryTag { get; set; }
+    }
+
+    public interface IPropagable<TContext> where TContext : Context
+    {
+        Expression<Func<TContext, bool>> InContexts(Propagation propagation);
+
+        TContext Apply(TContext context, Propagation propagation);
+
+        bool CanApply(Propagation propagation);
     }
 }
---
+++ source/Packages.Microservices/Domain/Replication.cs
@@ -0,0 +1,20 @@
+﻿using Packages.Microservices.Messages;
+
+namespace Packages.Microservices.Domain
+{
+    public class Replication : IReceivable
+    {
+        public string? Id { get; set; }
+
+        public dynamic? Content { get; set; }
+
+        public ulong DeliveryTag { get; set; }
+    }
+
+    public interface IReplication<TContext> where TContext : Context
+    {
+        TContext Save(Replication replication);
+
+        bool CanSave(Replication propagation);
+    }
+}
